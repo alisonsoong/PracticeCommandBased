7a170520e0644988a2cfb0b8431141f294417c99
Alison Soong <alisonsoong@gmail.com>
2021-11-17 10:08:19 -0800

    init

---
+++ src/main/cpp/subsystems/Drivetrain.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/Drivetrain.h"
+
+Drivetrain::Drivetrain() {
+  // Implementation of subsystem constructor goes here.
+}
+
+void Drivetrain::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+}
+
+void Drivetrain::SimulationPeriodic() {
+  // Implementation of subsystem simulation periodic method goes here.
+}
